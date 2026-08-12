cbf6ec0af671604761a78cb1833f42cfe4520d9c
saivamsisinguru <154603612+saivamsisinguru@users.noreply.github.com>
2026-08-12 23:13:09 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,3 +10,22 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(100))
     city = db.Column(db.String(100))
     is_lawyer = db.Column(db.Boolean, default=False)
+
+class Case(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    advocate_name = db.Column(db.String(100))
+    legal_area = db.Column(db.String(50))
+    sub_type = db.Column(db.String(50))
+    city = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    status = db.Column(db.String(50), default='Active')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Fee tracking
+    advocate_fee = db.Column(db.Integer, default=0)
+    platform_fee = db.Column(db.Integer, default=0)
+    total_paid = db.Column(db.Integer, default=0)
+    
+    def __repr__(self):
+        return f'<Case {self.id}: {self.legal_area} - {self.sub_type}>'
